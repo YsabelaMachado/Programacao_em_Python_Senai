88f46cc2cf8f7ebbe7a85bce26a3368f56bd1ca4
Ysabela Machado <ysabelamachado321@gmail.com>
2025-10-04 17:40:49 -0300

    Add files via upload

---
+++ AULA_3_LINGUAGEM/sinais.py
@@ -0,0 +1,14 @@
+#sinais de comparação aritméticos
+
+print("+ - * / % // **")
+
+n1 = 10
+n2 = 30
+print(n1 + n2)
+print(n1 - n2)
+print(n1 * n2)
+print(n1 / n2)
+
+print(n1 % n2)
+print(n1 // n2)
+print(n1 ** n2)
